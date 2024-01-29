2cdfca59eaf0d2c28f9bf0fee28787e3cc612ab2
Jaspinder Singh <jaspinderkohli@gmail.com>
2024-01-29 19:14:36 -0330

    changing App.js for test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "John Doe";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>  
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1> Hello {name}!</h1>
+      <p> Lorem ispsum </p>
     </div>
+    </>
   );
 }
 
