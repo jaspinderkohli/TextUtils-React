a5818e0fafa2044dc3009d1de8ef0702751a8330
Jaspinder Singh <jaspinderkohli@gmail.com>
2024-02-04 14:26:18 -0330

    added dark mode for background

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light');
+  
+  const toggleMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light');
+    document.body.style.backgroundColor = mode === 'light' ? '#042743' : 'white';
+  }
+
   return (
     <>  
       {/* Navbar component */}
-      <Navbar title="TextUtils" aboutText="About TextUtils"/>
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below"/>
+        <TextForm heading="Enter the text to analyze below" mode={mode}/>
         {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -44,11 +44,10 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h1>{props.heading} </h1>
             <div className="mb-3">
-                <textarea className="form-control" onChange={handleOnChange} id="myBox" rows="8" value={text}>
-
+                <textarea className="form-control" onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} value={text} id="myBox" rows="8">
                 </textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
@@ -57,12 +56,12 @@ export default function TextForm(props) {
             <button className="btn btn-info mx-2" onClick={handleLocation}>Locate</button>
             <button className="btn btn-info mx-2" onClick={handleCopy}>Copy</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
             <p> {text.split(" ").length} words and {text.length} characters</p>
             <p> {0.008 * text.split(" ").length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0 ? text: "Enter something to preview here"}</p>
         </div>
         </>
     )
